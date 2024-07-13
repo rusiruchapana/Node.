b45204a20787dbde34a264557d1e108a3ab47f3b
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-13 14:10:34 +0530

    Update index.js

--- 5/index.js
+++ 5/index.js
@@ -1,1 +1,10 @@
-console.log("rusiru chapana dddd");
+const superagent = require("superagent");
+
+superagent
+    .get("https://dog.ceo/api/breeds/image/random") 
+    .end((err,res)=>{
+        if (err) {
+            console.error(err);
+        }
+        console.log(res.body.message);
+    });
