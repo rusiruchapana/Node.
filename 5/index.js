249fc881c9f1f8186125f7e8ef64a84ce67d1cd1
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-13 20:55:14 +0530

    writing files using promises.

--- 5/index.js
+++ 5/index.js
@@ -13,6 +13,21 @@ const read_promise=(file)=>{
     })
 }
 
+const write_promise=(message)=>{
+    return new Promise((resolve, reject) => {
+        fs.writeFile("./dogs-images",message,err=>{
+            if (err) {
+                reject(err);
+            }else{
+                console.log("Succesfully write.");
+                resolve(message);
+            }
+        });
+    })
+}
+
+
+
 
 
 
@@ -21,8 +36,12 @@ read_promise("./dog.txt")
     .then(data=>{
         return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);      
     })
-    .then(data=>{
-        console.log(data.body);
+    .then(data1=>{
+        return data1.body;
+    })
+    .then((data2)=>{
+        //console.log(data2.message);
+        write_promise((data2.message));
     })
     .catch(err=>{
         console.error("Error",err);
