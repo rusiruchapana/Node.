241075e4b1f06b29a2fb74edde86c2333dc95e4e
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-13 14:31:25 +0530

    getting images using api's and save them in files.

--- 5/index.js
+++ 5/index.js
@@ -1,10 +1,29 @@
 const superagent = require("superagent");
+const fs = require("fs");
 
-superagent
-    .get("https://dog.ceo/api/breeds/image/random") 
-    .end((err,res)=>{
-        if (err) {
-            console.error(err);
-        }
-        console.log(res.body.message);
-    });
+fs.readFile("./dog.txt","utf8",(err,data)=>{
+    if (err) {
+        console.error(err);
+    }else{
+        superagent
+            .get(`https://dog.ceo/api/breed/${data}/images/random`) 
+            .end((err,res)=>{
+                const img_url=res.body.message;
+                if (err) {
+                    console.error(err);
+                }else{
+                    fs.writeFile("./dogs-images",img_url,(err)=>{
+                            if (err) {
+                                console.error(err);
+                            }else{
+                                console.log("Succesfully write file.");
+                            }
+                    });
+                }
+                
+            });
+    }
+});
+
+
+
