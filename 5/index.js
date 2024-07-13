c02af06646766f69f52a36255bab3c1a6949e109
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-13 20:20:03 +0530

    using promise read files.

--- 5/index.js
+++ 5/index.js
@@ -1,29 +1,27 @@
 const superagent = require("superagent");
 const fs = require("fs");
 
-fs.readFile("./dog.txt","utf8",(err,data)=>{
-    if (err) {
-        console.error(err);
-    }else{
-        superagent
-            .get(`https://dog.ceo/api/breed/${data}/images/random`) 
-            .end((err,res)=>{
-                const img_url=res.body.message;
-                if (err) {
-                    console.error(err);
-                }else{
-                    fs.writeFile("./dogs-images",img_url,(err)=>{
-                            if (err) {
-                                console.error(err);
-                            }else{
-                                console.log("Succesfully write file.");
-                            }
-                    });
-                }
-                
-            });
-    }
-});
+const read_promise=(file)=>{
+    return new Promise((resolve, reject) => {
+        fs.readFile(file,"utf8",(err,data)=>{
+            if(err){
+                reject(err);
+            }else{
+                resolve(data);
+            }
+        });
+    })
+}
+
+
+read_promise("./dog.txt")
+    .then(data=>{
+        console.log(data);
+        return data;
+    })
+    .catch(err=>{
+        console.error("Error",err);
+    });
 
 
 
