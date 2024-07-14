5ade2a3b77a44135182d35a05d9496b199628de7
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-14 13:10:30 +0530

    get jsons data from api's.

--- 6/app.js
+++ 6/app.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require("fs");
 
 
-app.get("/",(req,res)=>{
-    res.send("Get request");
+const tours_details = fs.readFileSync("./dev-data/data/tours-simple.json","utf8");
+const tours_details_json = JSON.parse(tours_details);
+
+
+app.get("/api/v1/tours",(req,res)=>{
+    res.send({
+        status: "200 Ok",
+        data: {
+            tours: tours_details_json
+        }
+
+    });
 }); 
+
 
 
 app.listen(port, () => {
