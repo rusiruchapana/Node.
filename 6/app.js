bbe7d391a146c2e8297a85d8d56ae549cf4b073d
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-17 21:10:53 +0530

    connect to mongodb atlas cloud database.

--- 6/app.js
+++ 6/app.js
@@ -6,6 +6,20 @@ require("dotenv").config();
 const port = process.env.PORT;
 
 
+//connect to mongodb atlas cloud database.
+const mongoose = require("mongoose");
+const url = "mongodb+srv://rusiru:" + process.env.PASS + "@isuru.yeeakum.mongodb.net/tours?retryWrites=true&w=majority&appName=Isuru";
+
+mongoose.connect(url)
+    .then(()=>{
+        console.log("Database succesfully created.");
+    })
+;
+
+
+
+
+
 //time
 app.use((req,res,next)=>{
     req.requestTime = new Date().toISOString();
