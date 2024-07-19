5766469639b63f7d36f9ae51b0ae212ad9e4e508
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-19 13:07:59 +0530

    connect to the mongodb database.

--- 8/app.js
+++ 8/app.js
@@ -3,9 +3,22 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
 
+//connect to the mongodb database.
+const mongoose = require("mongoose");
+const db_url = "mongodb+srv://rusiru:rusirubB@rusiru.fokxvs9.mongodb.net/natour?retryWrites=true&w=majority&appName=rusiru";
+mongoose.connect(db_url)
+        .then(()=>{
+            console.log("Succesfully connected to the database.");
+        })
+        .catch((err)=>{
+            console.log("Error: ", err);
+        });
+
 
 const tours_routes = require("./Routes/tours");
 app.use("/tours",tours_routes);
+
+
 
 
 app.listen(port, ()=>{
