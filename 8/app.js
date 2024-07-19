e2563c6fbc158810c60edc20be3a5fe8aadbdb77
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-19 12:22:15 +0530

    getting secret data from the .env file.

--- 8/app.js
+++ 8/app.js
@@ -1,12 +1,13 @@
 const express =  require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT;
 
 
 
 
 app.listen(port, ()=>{
-    console.log("Server is started.");
+    console.log(`Server is started on ${port}`);
 });
 
 
