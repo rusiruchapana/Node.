9924b214e98f0c55940678faac31684d35955b69
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-22 23:54:09 +0530

    modeling the tours.

--- 8/Models/tourModel.js
+++ 8/Models/tourModel.js
@@ -5,13 +5,55 @@ const mongoose = require("mongoose");
 const user_schema = new mongoose.Schema({
     name:{
         type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    duration:{
+        type:Number,
+        required:[true,"A tour must have a duration."]
+    },
+    maxGroupSize:{
+        type:Number,
         required:true
     },
-    email:{
+    difficulty:{
+        type:String,
+        required:[true,"A tour must have a difficulty."]
+    },
+    ratingsAverage:{
+        type:Number,
+        default:4.5
+    },
+    ratingsQuantity:{
+        type:Number,
+        default:0
+    },
+    price:{
+        type:Number,
+        required:[true,"A tour must have a price."]
+    },
+    priceDiscount:Number,
+    summary:{
         type:String,
         required:true,
-        unique:true
-    }
+        trim:true
+    },
+    description:{
+        type:String,
+        trim:true
+    },
+    imageCover:{
+        type:String,
+        required:[true,"A tour must have a cover image."]
+    } ,
+    images: [String],
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+    startDates: [Date]
+        
 });
 
 const tour = mongoose.model("tour_details",user_schema);
