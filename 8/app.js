e5e3ba59e025d30e9b031d8cf4a59655f0b5ea6b
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-23 13:32:11 +0530

    create function to delete all customers.

--- 8/app.js
+++ 8/app.js
@@ -3,6 +3,12 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
 app.use(express.json());
+const fs = require("node:fs/promises");
+const tour_details = require("./Models/tourModel");
+
+
+
+
 
 //connect to the mongodb database.
 const mongoose = require("mongoose");
@@ -18,6 +24,33 @@ mongoose.connect(db_url)
 
 
 
+//read all sample datas.
+async function read_data(){
+    try {
+        const data = await fs.readFile("./dev-data/data/tours-simple.json","utf8");
+        console.log(data);
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
+//read_data();
+
+
+//delete all datas in the database.
+async function delete_data(){
+    try {
+        const delete_data = await tour_details.deleteMany({});
+        console.log("Deleted succesfully.");
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
+
+//delete_data();
+
+
+
+
 
 
 
@@ -28,6 +61,7 @@ app.use("/tours",tours_routes);
 
 
 app.listen(port, ()=>{
+    
     console.log(`Server is started on ${port}`);
 });
 
